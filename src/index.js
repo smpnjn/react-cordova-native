ae67dc694e4595aaa33e29b30fda79a3d516f7c9
Johnny Simpson <johnnysimpson@johnnys-mbp.home>
2021-05-14 17:42:43 +0100

    Initial push

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const startApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if(!window.cordova) {
+  startApp()
+} else {
+  document.addEventListener('deviceready', startApp, false)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
